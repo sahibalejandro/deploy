83e68935dbc7ae9ad3b78d7f447189293bf1f387
Sahib J. Leo <sahib.alejandro@gmail.com>
2018-10-06 21:21:53 -0500

    Execute script to create database on mysql when creating a new database through the rest API.

--- app/Http/Controllers/DatabasesController.php
+++ app/Http/Controllers/DatabasesController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Database;
 use Illuminate\Http\Request;
+use Symfony\Component\Process\Process;
 
 class DatabasesController extends Controller
 {
@@ -14,6 +15,22 @@ class DatabasesController extends Controller
 
     public function store(Request $request)
     {
+        $process = new Process([
+            './scripts/create-database',
+            $request->name,
+            $request->user,
+            $request->password,
+        ]);
+
+        try {
+            $process->mustRun();
+        } catch (\Exception $e) {
+            return response()->json(
+                ['error' => "Unable to create database. ({$e->getMessage()})"],
+                500
+            );
+        }
+
         $database = new Database($request->only('name', 'user'));
         auth()->user()->databases()->save($database);
         return response()->json($database, 201);
--- tests/Feature/DatabasesTest.php
+++ tests/Feature/DatabasesTest.php
@@ -15,24 +15,41 @@ class DatabasesTest extends TestCase
     public function it_creates_a_new_database()
     {
         $user = factory(User::class)->create();
+        $dbName = '_test_database';
+        $dbUser = '_test_user';
+        $dbPassword = 'secret';
 
         $response = $this->actingAs($user)
             ->post('/api/databases', [
-                'name' => 'my_database',
-                'user' => 'my_user',
+                'name' => $dbName,
+                'user' => $dbUser,
+                'password' => $dbPassword,
             ]);
 
         $response->assertStatus(201)
             ->assertJson([
                 'user_id' => $user->id,
-                'name' => 'my_database',
-                'user' => 'my_user',
+                'name' => $dbName,
+                'user' => $dbUser,
             ]);
 
         $this->assertDatabaseHas('databases', [
             'user_id' => $user->id,
-            'name' => 'my_database',
-            'user' => 'my_user',
+            'name' => $dbName,
+            'user' => $dbUser,
         ]);
+
+        $this->assertDatabaseConnection($dbName, $dbUser, $dbPassword);
+    }
+
+    protected function assertDatabaseConnection($database, $user, $password)
+    {
+        try {
+            $pdo = new \PDO("mysql:dbname={$database};host=127.0.0.1", $user, $password);
+        } catch (\PDOException $e) {
+            $this->fail("Unable to connect to database \"{$database}\". ({$e->getMessage()})");
+        }
+
+        return $this;
     }
 }
