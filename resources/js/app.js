23f8dc7891d3c6130868ea33adbbb900301d2e31
Sahib J. Leo <sahib.alejandro@gmail.com>
2018-11-22 23:17:57 -0600

    Added Portal Vue.

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,7 @@
 import './bootstrap';
 
 import Vue from 'vue';
+import PortalVue from 'portal-vue';
 import router from './router';
 import store from './store';
 import './vue-directives';
@@ -9,6 +10,8 @@ import './vue-global-mixin';
 import TheHeader from './components/the-header.vue';
 import TheFooter from './components/the-footer.vue';
 import AlertMessage from './components/alert-message.vue';
+
+Vue.use(PortalVue);
 
 const app = new Vue({
     store,
