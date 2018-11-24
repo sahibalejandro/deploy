724943e337f01772e078dd104dfb67081d440d38
Sahib J. Leo <sahib.alejandro@gmail.com>
2018-11-24 00:00:27 -0600

    Removed javascript tests

--- tests/JavaScript/setup.js
+++
@@ -1,5 +0,0 @@
-require('jsdom-global')();
-global.expect = require('expect');
-
-// Temporary bug fix, should be removed after vue-test-utils fixes #936
-window.Date = Date;
--- tests/JavaScript/webpack.config.js
+++
@@ -1,7 +0,0 @@
-let nodeExternals = require('webpack-node-externals');
-let laravelMixConfig = require('../../node_modules/laravel-mix/setup/webpack.config.js');
-
-laravelMixConfig.externals = [nodeExternals()];
-laravelMixConfig.devtool = 'inline-cheap-module-source-map';
-
-module.exports = laravelMixConfig;
